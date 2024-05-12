632451fcd12dcce60073b819b5261eb7bb31636f
Lucas Tomaszewski <luki22@gmail.com>
2024-05-12 19:31:08 +0200

    Added: missing action for success screen

--- shared/src/commonMain/kotlin/com/growgenie/kidsyCore/model/screenState/screen/onboarding/OnboardingSuccessState.kt
+++ shared/src/commonMain/kotlin/com/growgenie/kidsyCore/model/screenState/screen/onboarding/OnboardingSuccessState.kt
@@ -2,11 +2,16 @@ package com.growgenie.kidsyCore.model.screenState.screen.onboarding
 
 import com.growgenie.kidsyCore.model.screenState.ScreenName
 import com.growgenie.kidsyCore.model.screenState.ScreenState
+import com.growgenie.kidsyCore.model.screenState.UserAction
 
 data class OnboardingSuccessState(val state: State = State.NORMAL): ScreenState {
     override val screenName: ScreenName = ScreenName.ONBOARDING_SUCCESS
     enum class State {
         NORMAL
+    }
+
+    enum class Action: UserAction {
+        CREATE_ACCOUNT, LOG_IN
     }
 
     val backgroundColor: String = "#FFE9FE"
